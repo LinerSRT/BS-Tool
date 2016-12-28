4e36b8c4416459dfedf36cbf96f179c1a5659e80
Yuri <SeriniTY320@gmail.com>
2016-12-29 00:47:23 +0300

    Update: now can compile kernels too)))

--- main.sh
+++ main.sh
@@ -3,6 +3,11 @@ export curdate=`date "+_%H:%M"`
 #Variables
 OUT_DIR="out"
 source initialization.bsf
+if [ -z initialization.bsf ]
+	then
+	config_script
+fi
+	
 config_script(){
 if [ ! -f initialization.bsf ]
   then
@@ -28,6 +33,7 @@ if [ ! -f initialization.bsf ]
 		SUBLEVEL=$(grep -oP 'SUBLEVEL = \K.*'  Makefile)
 	fi
 	echo "KRN_VER='3.$PATCHLEVEL.$SUBLEVEL'" >> initialization.bsf
+	echo "CHKSV='$PATCHLEVEL'" >> initialization.bsf
 
 	#Setting project name
 	PJ_NAME=$(whiptail --title "BS Tool" --inputbox "Set project name" 10 60 3>&1 1>&2 2>&3)
@@ -38,7 +44,7 @@ if [ ! -f initialization.bsf ]
 	echo "NUMBER_OF_CORES='$N_CORES'" >> initialization.bsf
 	
 	#PATH's
-	if [ "$PATCHLEVEL" -eq "4" ]
+	if [ "$CHKSV" -eq "4" ]
 	 then
 		KERN_IMG=$OUT_DIR/target/product/$PJ_NAME/obj/KERNEL_OBJ/arch/arm/boot/zImage
 		mkdir $OUT_DIR/kernel
@@ -51,6 +57,9 @@ if [ ! -f initialization.bsf ]
 	mkdir $OUT_DIR/logs
 	
 	#Configuring script
+	ARCH=$(whiptail --title "BS Tool" --inputbox "Set ARCH" 10 60 3>&1 1>&2 2>&3)
+	echo "ARCH='$PJ_NAME'" >> initialization.bsf
+
 	if(whiptail --title  "BS Tool" --yesno  "Download BS Tool recommended cross-compile tool?" 10 60)
 	then
 		echo "USE_OTHER_CROSS_COMPILER='yes'" >> initialization.bsf
@@ -72,13 +81,6 @@ if [ ! -f initialization.bsf ]
 		echo "PACK_IMG_AFTER_BUILD='no'" >> initialization.bsf
 	fi
 
-	if(whiptail --title  "BS Tool" --yesno  "Open kernel.log after fail compilation?" 10 60)
-	then
-		echo "FAIL_LOG='yes'" >> initialization.bsf
-	else
-		echo "FAIL_LOG='no'" >> initialization.bsf
-	fi
-
 	if(whiptail --title  "BS Tool" --yesno  "Download packages for compiling?" 10 60)
 	then
 		echo "ADD_PKG='yes'" >> initialization.bsf
@@ -92,6 +94,20 @@ if [ ! -f initialization.bsf ]
 	else
 		echo "STORE_LOG_DB='no'" >> initialization.bsf
 	fi
+
+	if(whiptail --title  "BS Tool" --yesno  "Auto-open log afterr compilation error?" 10 60)
+	then
+		echo "LOG_OPEN='yes'" >> initialization.bsf
+	else
+		echo "LOG_OPEN='no'" >> initialization.bsf
+	fi
+
+	if(whiptail --title  "BS Tool" --yesno  "Create tools folder in $OUT_DIR?" 10 60)
+	then
+		echo "TOOL_FOLDER='yes'" >> initialization.bsf
+	else
+		echo "TOOL_FOLDER='no'" >> initialization.bsf
+	fi
  fi
 main_menu
 }
@@ -101,6 +117,13 @@ main_menu
         echo $i
     done
 } | whiptail --gauge "Loading..." 6 60 0
+#Tool dir
+if [ $TOOL_FOLDER = "yes" ]
+	then
+	mkdir $OUT_DIR/tools
+	else
+	rm -rf $OUT_DIR/tools
+#-------
 #-----------------------Functions---------------------------------------
 get_logcat(){
 	read LOGCAT < <(adb logcat)
@@ -137,11 +160,141 @@ get_device_info(){
 	echo "RAM_PHONE_KB='$RAM_PHONE_KB'" >> initialization.bsf
 	echo "KERN_VER_PHONE='$KERN_VER_PHONE'" >> initialization.bsf
 	echo "MANUFACTURER='$MANUFACTURER'" >> initialization.bsf
+	whiptail --title "BS Tool" --msgbox "CPU:$CPU_PHONE
+RAM(kb):$RAM_PHONE_KB
+Kernel version:$KERN_VER_PHONE
+Manufacturer:$MANUFACTURER" 10 60
+}
+compile_new_kernel(){
+if [ "$CHKSV" -eq "4" ]
+	 then
+		whiptail --title "BS Tool" --msgbox "Start making new $KRN_VER kernel for $PROJECT_NAME" 10 60
+		BUILD_START=$(date +"%s")
+		./makeMtk $PROJECT_NAME n k > /dev/null 2>&1
+		if [ $? -eq 0 ]; then
+			cp $KERN_IMG $OUT_DIR/kernel
+			BUILD_END=$(date +"%s")
+			#next use bs-packer? y/n ----------------------
+			DIFF=$(($BUILD_END - $BUILD_START))
+			whiptail --title "BS Tool" --msgbox "Kernel compiled in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds" 10 60
+		else
+			whiptail --title "BS Tool" --msgbox "Error when compiling kernel!!!" 10 60
+			#next open log??-----------------------
+			if [ $LOG_OPEN = "yes" ]
+				then
+				less out/target/product/$PROJECT_NAME\_kernel.log
+			else
+			whiptail --title "BS Tool" --msgbox "If you want to auto-open log, please re-config script. Or edit manualy config file."
+			if [ $STORE_LOG_DB = "no" ]
+				then
+				rm out/target/product/$PROJECT_NAME\_kernel.log
+			fi
+			fi
+		fi
+	 else
+		whiptail --title "BS Tool" --msgbox "Start making new $KRN_VER kernel for $PROJECT_NAME" 10 60
+		BUILD_START=$(date +"%s")
+		export ARCH=$ARCH
+		make $PROJECT_NAME\_defconfig
+		make -j$NUMBER_OF_CORES | tee make.log
+		if [ $? -eq 0 ]; then
+			BUILD_END=$(date +"%s")
+			DIFF=$(($BUILD_END - $BUILD_START))
+			cp $KERN_IMG $OUT_DIR/kernel
+			whiptail --title "BS Tool" --msgbox "Kernel compiled in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds" 10 60
+		else
+			whiptail --title "BS Tool" --msgbox "Error when compiling kernel!!!" 10 60
+			#next open log??-----------------------
+			if [ $LOG_OPEN = "yes" ]
+				then
+				less make.log
+			else
+			whiptail --title "BS Tool" --msgbox "If you want to auto-open log, please re-config script. Or edit manualy config file."
+			if [ $STORE_LOG_DB = "no" ]
+				then
+				rm make.log
+			fi
+			fi
+		fi
+			
+	fi
+}
+recompile_kernel(){
+if [ "$CHKSV" -eq "4" ]
+	 then
+		whiptail --title "BS Tool" --msgbox "Start making new $KRN_VER kernel for $PROJECT_NAME" 10 60
+		BUILD_START=$(date +"%s")
+		./makeMtk $PROJECT_NAME r k > /dev/null 2>&1
+		if [ $? -eq 0 ]; then
+			cp $KERN_IMG $OUT_DIR/kernel
+			BUILD_END=$(date +"%s")
+			#next use bs-packer? y/n ----------------------
+			DIFF=$(($BUILD_END - $BUILD_START))
+			whiptail --title "BS Tool" --msgbox "Kernel compiled in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds" 10 60
+		else
+			whiptail --title "BS Tool" --msgbox "Error when compiling kernel!!!" 10 60
+			#next open log??-----------------------
+			if [ $LOG_OPEN = "yes" ]
+				then
+				less out/target/product/$PROJECT_NAME\_kernel.log
+			else
+			whiptail --title "BS Tool" --msgbox "If you want to auto-open log, please re-config script. Or edit manualy config file."
+			if [ $STORE_LOG_DB = "no" ]
+				then
+				rm out/target/product/$PROJECT_NAME\_kernel.log
+			fi
+			fi
+		fi
+	 else
+		whiptail --title "BS Tool" --msgbox "Start making new $KRN_VER kernel for $PROJECT_NAME" 10 60
+		BUILD_START=$(date +"%s")
+		export ARCH=$ARCH
+		make $PROJECT_NAME\_defconfig
+		make -j$NUMBER_OF_CORES | tee make.log
+		if [ $? -eq 0 ]; then
+			BUILD_END=$(date +"%s")
+			DIFF=$(($BUILD_END - $BUILD_START))
+			cp $KERN_IMG $OUT_DIR/kernel
+			whiptail --title "BS Tool" --msgbox "Kernel compiled in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds" 10 60
+		else
+			whiptail --title "BS Tool" --msgbox "Error when compiling kernel!!!" 10 60
+			#next open log??-----------------------
+			if [ $LOG_OPEN = "yes" ]
+				then
+				less make.log
+			else
+			whiptail --title "BS Tool" --msgbox "If you want to auto-open log, please re-config script. Or edit manualy config file."
+			if [ $STORE_LOG_DB = "no" ]
+				then
+				rm make.log
+			fi
+			fi
+		fi
+			
+	fi
+}
+clear_out(){
+if [ "$CHKSV" -eq "4" ]
+	 then
+	rm -rf /out
+	else
+	find -name '*.o' -delete;
+fi	
+}
+clean_kernel(){
+if [ "$CHKSV" -eq "4" ]
+	 then
+	cd kernel
+	make TARGET_PRODUCT=$PROJECT_NAME clear
+	make TARGET_PRODUCT=$PROJECT_NAME mrproper
+	cd ..
+	find -name '*.o' -delete;
+	else
+	make clean
+	find -name '*.o' -delete;
+fi
 }
 #-----------------------MENU Construct----------------------------------
-remount_sys(){
-
-}
 log_menu(){
 LOG_MENU=$(whiptail --title  "BS Tool" --menu  "	" 10 60 4 \
 "1" "Get logcat" \
@@ -218,20 +371,35 @@ fi
 }
 
 compile_menu(){
-COMPILE_MENU=$(whiptail --title  "BS Tool" --menu  "	" 10 60 4 \
+COMPILE_MENU=$(whiptail --title  "BS Tool" --menu  "	" 11 60 5 \
 "1" "Compile new kernel" \
 "2" "Re-compile kernel" \
 "3" "Clean out directory" \
-"4" "Back"  3>&1 1>&2 2>&3)
+"4" "Clean kernel dir" \
+"5" "Back"  3>&1 1>&2 2>&3)
+
+if [ $COMPILE_MENU -eq "5" ]
+	then
+	main_finctions
+fi
+if [ $COMPILE_MENU -eq "1" ]
+	then
+	compile_new_kernel
+fi
+if [ $COMPILE_MENU -eq "2" ]
+	then
+	recompile_kernel
+fi
+
+if [ $COMPILE_MENU -eq "3" ]
+	then
+	clear_out
+	fi
 
 if [ $COMPILE_MENU -eq "4" ]
 	then
-	main_finctions
-fi
-if [ $COMPILE_MENU -eq "4" ]
-	then
-	main_finctions
-fi
+	clean_kernel
+	fi
 }
 
 flash_menu(){
@@ -278,7 +446,7 @@ fi
 
 settigs_menu(){
 SETTINGS_MENU=$(whiptail --title  "BS Tool" --menu  "	" 10 60 4 \
-"1" "Download BS Boot-packer add-on manual " \
+"1" "Download BS Boot-packer add-on " \
 "2" "Re-config script" \
 "3" "Back"  3>&1 1>&2 2>&3)
 if [ $SETTINGS_MENU -eq "3" ]
